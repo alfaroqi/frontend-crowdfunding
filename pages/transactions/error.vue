14be1a79c903eb27a21f554759c98739e0b3dab5
alfaroqi <roqialfaroqi@mail.com>
2022-01-01 15:00:41 +0700

    finish, error, unfinish page

---
+++ pages/transactions/error.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container mx-auto h-screen flex justify-center items-center">
+    <div class="w-full lg:w-1/3 px-10 lg:px-0">
+      <!-- <div class="flex justify-center items-center mx-auto mt-6 mb-8">
+        <img src="/funded-illustration.svg" alt="" class="w-full" />
+      </div> -->
+      <h2 class="font-medium mb-3 text-3xl text-center">
+        Oops. Something went wrong!
+      </h2>
+      <p class="text-center font-light">
+        An error accured when processing your transaction. <br />
+        Please try again later.
+      </p>
+
+      <div class="mb-4 mt-6">
+        <div class="mb-3">
+          <button
+            @click="$router.push({ path: '/' })"
+            class="
+              block
+              w-full
+              bg-orange-button
+              hover:bg-green-button
+              text-white
+              font-semibold
+              px-6
+              py-4
+              text-lg
+              rounded-full
+            "
+          >
+            Fund Other Project
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'success',
+}
+</script>
+
+
---
+++ pages/transactions/unfinish.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container mx-auto h-screen flex justify-center items-center">
+    <div class="w-full lg:w-1/3 px-10 lg:px-0">
+      <!-- <div class="flex justify-center items-center mx-auto mt-6 mb-8">
+        <img src="/funded-illustration.svg" alt="" class="w-full" />
+      </div> -->
+      <h2 class="font-medium mb-3 text-3xl text-center">
+        Transaction Unfinished
+      </h2>
+      <p class="text-center font-light">Your transaction has been cancelled.</p>
+
+      <div class="mb-4 mt-6">
+        <div class="mb-3">
+          <button
+            @click="$router.push({ path: '/' })"
+            class="
+              block
+              w-full
+              bg-orange-button
+              hover:bg-green-button
+              text-white
+              font-semibold
+              px-6
+              py-4
+              text-lg
+              rounded-full
+            "
+          >
+            Fund Other Project
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'success',
+}
+</script>
+
+
